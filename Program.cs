c349a6f52a512ebfe351500ee7684f4718c3a9cc
Benedikt Papiorek <benedikt@papiorek.de>
2024-09-28 03:49:58 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,63 @@
+using de.mydevtime.LicenseServer.Interfaces;
+using NuGet.Protocol.Plugins;
+
+namespace de.mydevtime.LicenseServer.Api;
+
+public class Program
+{
+    private static bool keepRunning = true;
+    
+    public static void Main(string[] args)
+    {
+        Console.CancelKeyPress += delegate(object? sender, ConsoleCancelEventArgs e) {
+            e.Cancel = true;
+            keepRunning = false;
+        };
+        
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddPluginFramework<IApiServerPlugin>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        {
+                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            TemperatureC = Random.Shared.Next(-20, 55),
+                            Summary = summaries[Random.Shared.Next(summaries.Length)]
+                        })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+        app.Run();
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿# License Server - API Server
+
+Rest API License Server. It can be used standalone.
+
+## Attribution
+
+This software is developed by Benedikt Papiorek.
+Any redistributions or modifications of this software must include proper attribution to the original author, as per the Apache License 2.0.
